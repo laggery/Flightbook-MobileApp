3cbca743758686b979df8589ebbaf4c6cffa2e4c
fabiangotzen <fabian.gotzen@gmail.com>
2021-02-13 16:49:08 +0100

    adjust app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -14,7 +14,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpErrorInterceptor } from './interceptor/error.interceptor';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-import { PlaceModule, FlightModule, GliderModule, NewsModule, AccountModule, HttpAuthInterceptor } from 'flightbook-commons-library';
+import {
+  AccountModule,
+  FlightModule,
+  GliderModule,
+  HttpAuthInterceptor,
+  NewsModule,
+  PlaceModule
+} from 'flightbook-commons-library';
+import { SharedModule } from "./shared/shared.module";
 
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -27,24 +35,26 @@ export function tokenGetter() {
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [
-    BrowserModule,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: (createTranslateLoader),
-        deps: [HttpClient]
-      }
-    }),
-    IonicModule.forRoot(),
-    FlightModule.forRoot(environment),
-    PlaceModule.forRoot(environment),
-    GliderModule.forRoot(environment),
-    NewsModule.forRoot(environment),
-    AccountModule.forRoot(environment),
-    HttpClientModule,
-    AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
+    imports: [
+        BrowserModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: (createTranslateLoader),
+                deps: [HttpClient]
+            }
+        }),
+        IonicModule.forRoot(),
+        FlightModule.forRoot(environment),
+        PlaceModule.forRoot(environment),
+        GliderModule.forRoot(environment),
+        NewsModule.forRoot(environment),
+        AccountModule.forRoot(environment),
+        HttpClientModule,
+        AppRoutingModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
+        SharedModule
+    ],
   providers: [
     StatusBar,
     SplashScreen,
